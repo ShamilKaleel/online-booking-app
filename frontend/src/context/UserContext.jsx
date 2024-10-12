9434673000a639771d5200239835a55b44de5267
Shamil Kaleel <155573196+ShamilKaleel@users.noreply.github.com>
2024-10-12 11:31:50 +0530

    vercel json  added to fronend

--- frontend/src/context/UserContext.jsx
+++ frontend/src/context/UserContext.jsx
@@ -14,6 +14,7 @@ export function UserContextProvider({ children }) {
         .then(({ data }) => {
           setUser(data);
           setReady(true);
+          console.log(data);
         })
         .catch(() => {
           setUser(null);
